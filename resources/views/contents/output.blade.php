5a0e7946561e94e8fe8c2a202eb2904ac4be040a
takatoshiinaoka <heavenhayao2316@gmail.com>
2021-10-30 12:09:50 +0900

    日本語化

--- resources/views/contents/output.blade.php
+++ resources/views/contents/output.blade.php
@@ -6,7 +6,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('一覧') }}
+      {{ __('めがね一覧') }}
     </h2>
   </x-slot>
   <div class="py-12">
--- resources/views/contents/searched.blade.php
+++ resources/views/contents/searched.blade.php
@@ -3,7 +3,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('検索結果') }}
+      {{ __('めがね検索結果') }}
     </h2>
   </x-slot>
   <div class="py-12">
