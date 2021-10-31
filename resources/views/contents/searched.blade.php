47a08815e2a58abe9f90088d0cac9861bb65e938
takatoshiinaoka <heavenhayao2316@gmail.com>
2021-10-31 13:27:12 +0900

    searhedの画像サイズ設定

--- resources/views/contents/searched.blade.php
+++ resources/views/contents/searched.blade.php
@@ -16,7 +16,7 @@
         <hr>
 
         @if (isset($item->file_path))
-        <img src="{{asset('storage/' . $item->file_path)}}" alt="{{asset('storage/' . $item->file_path)}}">
+        <img src="{{asset('storage/' . $item->file_path)}}" alt="{{asset('storage/' . $item->file_path)}}" width="200" height="150px">
         @endif
         
         <p>名前: {{$item->name}} 
